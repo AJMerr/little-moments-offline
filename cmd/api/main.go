e3c90b05a3155ad44dd1f155ca4fb98f0ba97805
Austin Merrill <austinm9506@gmail.com>
2025-08-15 03:49:58 -0400

    Pulled in the routing from interal/api and verified functionality

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,13 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/AJMerr/little-moments-offline/internal/api"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "This is a test")
-	})
+	// Sets a var for the Router
+	router := api.RouterHandler()
 
-	fmt.Println("Now listening on 127.0.0.1:8173")
-	log.Fatal(http.ListenAndServe(":8173", nil))
+	fmt.Println("Server starting on 127.0.0.1:8173")
+	err := http.ListenAndServe("127.0.0.1:8173", router)
+	if err != nil {
+		log.Fatalf("Server failed to start %v", err)
+	}
 }

51e343a8a1caef84d5aa19c99b60f616a52c3628
Austin Merrill <austinm9506@gmail.com>
2025-08-16 03:05:03 -0400

    Added references to the new schema

--- internal/db/migrate.go
+++ internal/db/migrate.go
@@ -3,7 +3,21 @@ package db
 import "gorm.io/gorm"
 
 func Migrate(gdb *gorm.DB) error {
-	return gdb.AutoMigrate(
+	if err := gdb.AutoMigrate(
 		&User{},
-	)
+		&Photo{},
+		&Album{},
+		&AlbumPhoto{},
+	); err != nil {
+		return err
+	}
+
+	if err := gdb.SetupJoinTable(&Album{}, "Photos", &AlbumPhoto{}); err != nil {
+		return err
+	}
+	if err := gdb.SetupJoinTable(&Photo{}, "Albums", &AlbumPhoto{}); err != nil {
+		return err
+	}
+
+	return nil
 }

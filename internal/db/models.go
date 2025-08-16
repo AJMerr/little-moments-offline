b0b66c987616b31c3f154d3bdcd088cceb498f18
Austin Merrill <austinm9506@gmail.com>
2025-08-16 03:04:05 -0400

    Created schema for photos and albums. Many to many relation between albums and photos and one to many between a user and photos + albums.

--- internal/db/models.go
+++ internal/db/models.go
@@ -9,4 +9,37 @@ type User struct {
 	Email     string    `gorm:"uniqueIndex;not null"`
 	UserName  string    `gorm:"type:text"`
 	CreatedAt time.Time `gorm:"not null"`
+
+	Photos []Photo `gorm:"foreignKey:OwnerID"`
+}
+
+type Photo struct {
+	ID          string `gorm:"primaryKey;type:text"`
+	OwnerID     string `gorm:"index;not null"`
+	Title       string `gorm:"type:text;not null"`
+	Description string `gorm:"type:text"`
+	OriginKey   string `gorm:"not null"`
+	ContentType string `gorm:"not null"`
+	Bytes       int64  `gorm:"not null"`
+	CreatedAt   time.Time
+
+	Owner  User    `gorm:"constraint:OnDelete:CASCADE;foreignKey:OwnerID;references:ID"`
+	Albums []Album `gorm:"many2many:album_photos"`
+}
+
+type Album struct {
+	ID          string `gorm:"primaryKey;type:text"`
+	OwnerID     string `gorm:"index;not null"`
+	Title       string `gorm:"type:text;not null"`
+	Description string `gorm:"type:text"`
+	CreatedAt   time.Time
+
+	Owner  User    `gorm:"constraint:OnDelete:CASCADE;foreignKey:OwnerID;references:ID"`
+	Photos []Photo `gorm:"many2many:album_photos"`
+}
+
+type AlbumPhoto struct {
+	AlbumID string    `gorm:"primaryKey;type:text;index"`
+	PhotoID string    `gorm:"primaryKey;type:text;index"`
+	AddedAt time.Time `gorm:"not null;index"`
 }

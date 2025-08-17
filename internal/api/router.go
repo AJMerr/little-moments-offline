f1991b751194770561957fe1dd72cec81b9c25ff
Austin Merrill <austinm9506@gmail.com>
2025-08-17 02:53:21 -0400

    Added /photos/presign router

--- internal/api/router.go
+++ internal/api/router.go
@@ -2,13 +2,16 @@ package api
 
 import (
 	"net/http"
+
+	"github.com/AJMerr/little-moments-offline/internal/storage"
 )
 
-func RouterHandler() http.Handler {
+func RouterHandler(s3 *storage.S3) http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /healthz", healthzHandler)
 	mux.HandleFunc("GET /version", versionHandler)
 	mux.HandleFunc("GET /panic", func(w http.ResponseWriter, r *http.Request) { panic("AAAAAAHHH BEES") })
+	mux.HandleFunc("POST /photos/presign", PresignPhoto(s3))
 	return reqID(logger(panicRecovery(mux)))
 }

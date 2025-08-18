34996b01db09ba57fdb3aeacec3cfae47c479609
Austin Merrill <austinm9506@gmail.com>
2025-08-18 02:49:56 -0400

    Added route /photos for getting all photos

--- internal/api/router.go
+++ internal/api/router.go
@@ -13,6 +13,7 @@ func RouterHandler(gdb *gorm.DB, s3 *storage.S3) http.Handler {
 	mux.HandleFunc("GET /healthz", healthzHandler)
 	mux.HandleFunc("GET /version", versionHandler)
 	mux.HandleFunc("GET /panic", func(w http.ResponseWriter, r *http.Request) { panic("AAAAAAHHH BEES") })
+	mux.HandleFunc("GET /photos", GetAllPhotos(gdb))
 	mux.HandleFunc("POST /photos/presign", PresignPhoto(s3))
 	mux.HandleFunc("POST /photos/confirm", ConfirmPhoto(gdb, s3))
 	return reqID(logger(panicRecovery(mux)))

4bc3999847bbd78c11c4b2d55d1d9bd06607e22e
Austin Merrill <austinm9506@gmail.com>
2025-08-17 23:54:08 -0400

    Added ConfirmPhoto function to confirm the presign

--- internal/api/photos.go
+++ internal/api/photos.go
@@ -8,8 +8,10 @@ import (
 	"strings"
 	"time"
 
+	db "github.com/AJMerr/little-moments-offline/internal/db"
 	"github.com/AJMerr/little-moments-offline/internal/storage"
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type presignReq struct {
@@ -62,6 +64,64 @@ func PresignPhoto(s3 *storage.S3) http.HandlerFunc {
 			URL:     url,
 			Key:     key,
 			Headers: headers,
+		})
+	}
+}
+
+type confirmReq struct {
+	Key         string `json:"string"`
+	Bytes       int64  `json:"bytes"`
+	ContentType string `json:"content-type"`
+	Title       string `json:"title,omitempty"`
+	Description string `json:"description,omitempty"`
+}
+
+// Function to confirm that a photo exists in MinIO
+func ConfirmPhoto(gdb *gorm.DB, s3 *storage.S3) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var in confirmReq
+		if err := json.NewDecoder(r.Body).Decode(&in); err != nil {
+			writeError(w, http.StatusBadRequest, "bad_request")
+			return
+		}
+		in.Key = strings.TrimSpace(in.Key)
+		in.ContentType = strings.TrimSpace(in.ContentType)
+		if in.Key == "" || in.ContentType == "" || in.Bytes < 0 {
+			writeError(w, http.StatusBadRequest, "missing_fields")
+			return
+		}
+
+		photo := db.Photo{
+			ID:          uuid.NewString(),
+			OwnerID:     "local_user", // TEMPORARY, WILL ADD AUTH USER
+			Title:       in.Title,
+			Description: in.Description,
+			OriginKey:   in.Key,
+			ContentType: in.ContentType,
+			Bytes:       in.Bytes,
+			CreatedAt:   time.Now(),
+		}
+
+		// Creates a row or returns existing key if it exists
+		if err := gdb.WithContext(r.Context()).Create(&photo).Error; err != nil {
+			// Tries to get existing key
+			var existingKey db.Photo
+			tx := gdb.WithContext(r.Context()).First(&existingKey, "original_key  = ?", in.Key)
+			if tx.Error == nil {
+				toJSON(w, http.StatusOK, map[string]any{
+					"id": existingKey.ID, "original_key": existingKey.OriginKey,
+					"bytes": existingKey.Bytes, "content_type": existingKey.ContentType,
+					"created_at": existingKey.CreatedAt,
+				})
+				return
+			}
+			writeError(w, http.StatusInternalServerError, "db_insert_failed")
+			return
+		}
+		toJSON(w, http.StatusCreated, map[string]any{
+			"id": photo.ID, "original_key": photo.OriginKey,
+			"bytes": photo.Bytes, "content_type": photo.ContentType,
+			"created_at": photo.CreatedAt,
 		})
 	}
 }
